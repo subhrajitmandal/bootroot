2403c7e104e43fd908cb8665add82a9c7963a0e8
subhrajitmandal <dipudipu2007@gmail.com>
2017-05-23 21:26:11 -0700

    Update home.php

--- home.php
+++ home.php
@@ -66,7 +66,7 @@
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <article <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <p><em>
